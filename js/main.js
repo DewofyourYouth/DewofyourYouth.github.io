9c4cda49be4170e0fbcaa38fe745f25d00645c48
dewofyouryouth <jacobshore@gmail.com>
2018-10-19 09:45:20 +0300

    nav-link

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $('.navbar-brand img').hover(function(){
     $(this).removeClass('animated shake');
 });
 
-$('.navbar-link').hover(function(){
+$('.nav-link').hover(function(){
     $(this).addClass('animated shake');
 }, function(){
     $(this).removeClass('animated shake');
