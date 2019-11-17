fea741c894d53eedc00e35b3563c70502d50e172
The Sarge <sergantspam@gmail.com>
2019-11-17 16:49:04 +0200

    hover

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-$('.navbar-brand img').hover(function(){
-    $(this).addClass('animated shake');
-}, function(){
-    $(this).removeClass('animated shake');
-});
+// $('.navbar-brand img').hover(function(){
+//     $(this).addClass('animated shake');
+// }, function(){
+//     $(this).removeClass('animated shake');
+// });
 
-$('a.nav-link').hover(function(){
-    $(this).addClass('animated shake');
-}, function(){
-    $(this).removeClass('animated shake');
-});
+// $('a.nav-link').hover(function(){
+//     $(this).addClass('animated shake');
+// }, function(){
+//     $(this).removeClass('animated shake');
+// });
