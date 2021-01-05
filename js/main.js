df68ed1edc4e35974d3912af3706e0e2156a07c0
Jacob Shore <jshore@datalign.net>
2021-01-05 10:00:45 +0200

    animate links

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
-// $('.navbar-brand img').hover(function(){
-//     $(this).addClass('animated shake');
-// }, function(){
-//     $(this).removeClass('animated shake');
-// });
+if ($(window).width() > 1000){
+    $('.navbar-brand img').hover(function(){
+        $(this).addClass('animated shake');
+    }, function(){
+        $(this).removeClass('animated shake');
+    });
+    
+    $('a.nav-link').hover(function(){
+        $(this).addClass('animated shake');
+    }, function(){
+        $(this).removeClass('animated shake');
+    });
 
-// $('a.nav-link').hover(function(){
-//     $(this).addClass('animated shake');
-// }, function(){
-//     $(this).removeClass('animated shake');
-// });
+}
